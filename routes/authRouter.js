0bae5bbfe09e15135947281f76a4f3d2f1c7ade3
Krislv4nk <kristinalev4enko89@gmail.com>
2024-09-18 18:56:40 +0200

    auth

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -12,13 +12,13 @@ import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", upload.single("avatar"), validateBody(userSignupSchema), authController.signup);
+authRouter.post("/signUp", upload.single("avatar"), validateBody(userSignupSchema), authController.signup);
 
-authRouter.post("/login", validateBody(userSigninSchema), authController.signin);
+authRouter.post("/signIn", validateBody(userSigninSchema), authController.signin);
 
 authRouter.get("/current", authenticate, authController.getCurrent);
 
-authRouter.post("/logout", authenticate, authController.signout);
+authRouter.post("/signOut", authenticate, authController.signout);
 
 authRouter.patch("/subscription", authenticate, authController.updateStatus);
 
