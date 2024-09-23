5f41b477494bb2e1802d0cc81a6e784591285abb
Krislv4nk <kristinalev4enko89@gmail.com>
2024-09-23 18:02:25 +0200

    fixed bookServices

--- services/booksServices.js
+++ services/booksServices.js
@@ -1,46 +1,56 @@
-import Book from "../models/Book.js";
 
+ 
+import HttpError from './../helpers/HttpError.js'; 
 
-
+const { OPEN_LIBRARY_URL_POPULAR, OPEN_LIBRARY_URL_BOOK, USER_AGENT } = process.env;
 
 export const getTopBooks = async (req, res) => {
   try {
-    const url = `https://openlibrary.org/subjects/popular.json?limit=30`;
+    const url = OPEN_LIBRARY_URL_POPULAR;
     const response = await fetch(url, {
       headers: {
-        'User-Agent': 'ReadJourney (kris_lv4nk@ukr.net)'
-      }
+        'User-Agent': USER_AGENT,
+      },
     });
-      const data = await response.json();
-      
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
 
     if (!data.works || data.works.length === 0) {
-      throw HttpError(404,'No popular books found');
+      throw new HttpError(404, 'No popular books found');
     }
 
     res.json(data.works);
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
-}
+};
 
 export const getBookByISBN = async (req, res) => {
   try {
     const { isbn } = req.params;
-    const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`;
+    const url = OPEN_LIBRARY_URL_BOOK.replace('${isbn}', isbn);
     const response = await fetch(url, {
       headers: {
-        'User-Agent': 'ReadJourney (kris_lv4nk@ukr.net)'
-      }
+        'User-Agent': USER_AGENT,
+      },
     });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
     const data = await response.json();
 
     if (!data[`ISBN:${isbn}`]) {
-      throw HttpError(404, 'Book not found' );
+      throw new HttpError(404, 'Book not found');
     }
 
     res.json(data[`ISBN:${isbn}`]);
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
