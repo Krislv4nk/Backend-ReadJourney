a43625e6f691f01ac22f40f85fe1037a8a90ea98
Krislv4nk <kristinalev4enko89@gmail.com>
2024-11-02 19:10:25 +0100

    BASE_URL fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,7 @@ const {JWT_SECRET,BASE_URL, JWT_REFRESH_SECRET, FRONTEND_BASE_URL} = process.env
     const verifyEmail = {
         to: email,
         subject: "Verify email",
-        html: `<a href="${FRONTEND_BASE_URL}/users/verify/${verificationToken}" target="_blank">Click to verify</a>`
+        html: `<a href="${BASE_URL}/users/verify/${verificationToken}" target="_blank">Click to verify</a>`
     };
 
     await sendEmail(verifyEmail);
@@ -81,7 +81,7 @@ if(user.verify) {
 const verifyEmail = {
     to: email,
     subject: "Verify email",
-    html: `<a href="${FRONTEND_BASE_URL}/verify/${user.verificationToken}" target="_blank">Click to verify</a>`
+    html: `<a href="${BASE_URL}/verify/${user.verificationToken}" target="_blank">Click to verify</a>`
 };
 
 await sendEmail(verifyEmail);
