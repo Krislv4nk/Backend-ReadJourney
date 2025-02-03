3e4337f16b3f10a7556b9ab6567887bdaae392ac
Krislv4nk <kristinalev4enko89@gmail.com>
2025-02-03 19:29:06 +0100

    fixed type in the userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -12,7 +12,7 @@ export const userSignupSchema = Joi.object({
 export const userSigninSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
-    favorites: [{ type: string }],
+    favorites: [{ type: String }],
 })
 
 export const userEmailSchema = Joi.object({
